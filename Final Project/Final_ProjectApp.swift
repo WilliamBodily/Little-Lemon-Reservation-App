5a27be5f4fe3974d31852cd2a2a980b5e6a04e0b
William Bodily <william.bodily@gmail.com>
2024-10-23 14:00:43 -0400

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by William Bodily on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
