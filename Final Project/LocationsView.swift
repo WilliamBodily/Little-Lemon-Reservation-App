4f0f8abdb3ba21a9062be289a1b166241992381c
William Bodily <william.bodily@gmail.com>
2024-10-28 14:32:10 -0400

    Adds information text and list of locations

--- Final Project/LocationsView.swift
+++ Final Project/LocationsView.swift
@@ -7,14 +7,18 @@ struct LocationsView: View {
             LittleLemonLogo()
                 .padding(.top, 50)
             
-            EmptyView() //2
+            Text((model.displayingReservationForm) ? "Reservation Details" : "Select a location")
                 .padding([.leading, .trailing], 40)
                 .padding([.top, .bottom], 8)
                 .background(Color.gray.opacity(0.2))
                 .cornerRadius(20)
                 
             NavigationView {
-                EmptyView() //3
+                List(model.restaurants, id: \.self) {
+                    restaurant in NavigationLink(destination: ReservationForm(restaurant)) {
+                        RestaurantView(restaurant)
+                    }
+                }
                 .navigationBarTitle("")
                 .navigationBarHidden(true)
             }
