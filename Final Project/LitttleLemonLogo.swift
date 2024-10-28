cde336fcef7f44e0b4661af67f0d86f0fdd63f51
William Bodily <william.bodily@gmail.com>
2024-10-28 12:39:25 -0400

    Initial commit

---
+++ Final Project/LitttleLemonLogo.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    
+    // Dreg Gesture
+    @State private var offsetValue:CGSize = .zero
+    var body: some View {
+        VStack {
+            EmptyView()
+        }
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
